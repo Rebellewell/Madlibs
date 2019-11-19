b39b5294f4b76642615d1da39fbc319397d5fdbd
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2019-11-19 11:48:19 -0700

    Worked on script file, added stories and backup word list; cannot get input words into array

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-console.log('Yello!!!');
+var storyBtn = document.getElementById('generate-story-button');
+var storyBox = document.getElementById('story-container');
+var wordInput = document.getElementsByClassName('word-type-select');
+var nounList = document.getElementById('nouns').value; 
+var words = [];
+
+storyBtn.addEventListener('click', storyHandler);
+
+function storyHandler() {
+  words.push(nounList);
+  var wordsArray = nounList.split(',');
+  console.log(wordsArray);
+}

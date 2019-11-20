65b6ede3bc721a789deaf200b8f866afc435b941
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2019-11-19 20:10:08 -0700

    able to create christmas story with input from user; working on changing the story based on radial button value

--- script.js
+++ script.js
@@ -1,11 +1,75 @@
 var storyBtn = document.getElementById('generate-story-button');
-var storyBox = document.getElementById('story-container');
-var wordInput = document.getElementsByClassName('word-type-select');
-var nounInput = document.getElementById('nouns'); 
-var words = [];
+var storyContainer = document.getElementById('story-container');
+var nounInputEl = document.getElementById('nouns'); 
 
 storyBtn.addEventListener('click', storyHandler);
 
 function storyHandler() {
-  console.log(nounInput.value)
+  var userWords = getUserWords();
+  console.log(userWords);
+
+
+// fill remaining space with backup words to get final owrds
+
+// grab the appropriate story string
+
+// fill the story string with our final words
+  var finalWords = getUserWords();  
+    
+    // nouns: ['NOUN','NOUN','NOUN','NOUN'],
+    // verbs: ['VERB','VERB','VERB','VERB'],
+    // adjectives: ['ADJECTIVE','ADJECTIVE','ADJECTIVE','ADJECTIVE'],
+    // adverbs: ['ADVERB','ADVERB','ADVERB','ADVERB']
+  
+  
+  var finalStory = getFilledStory(stories.christmas, finalWords);
+
+// show final story (filled out) in the DOM
+  displayStory(finalStory);
+}
+
+function getUserWords() {
+  return {
+    nouns: getUserWordsForType('nouns'),
+    verbs: getUserWordsForType('verbs'),
+    adjectives: getUserWordsForType('adjectives'),
+    adverbs: getUserWordsForType('adverbs')
+  }
+}
+
+// function getStory() {
+//   var isChristmas = document.getElementById('go-christmas').checked;
+//   var isShopping = document.getElementById('go-shopping').checked;
+//   var isBrainstorm = document.getElementById('go-brainstorm').checked;
+  
+//   if (isChristmas) {
+//     return stories.christmas;
+//   } else if (isShopping) {
+//     return stories.shopping;
+//   } else if (isBrainstorm) {
+//     return stories.brainstorm;
+//   }
+
+// get an array of words entered by user for this word type
+function getUserWordsForType(wordType) {
+  var rawStrInput = document.getElementById(wordType).value;
+  var inputWords = rawStrInput.split(',');
+  return inputWords;
+}
+
+// dispaly chosen story in DOM
+
+function displayStory(storyStr) {
+  storyContainer.innerText = storyStr;
+}
+
+// returns a new version of the story where ll the word spaces are filled in
+function getFilledStory(storyStr, finalWords) {
+  for (var i = 0; i < 4; i++) {
+    storyStr = storyStr.replace('{{noun}}', finalWords.nouns[i]);
+    storyStr = storyStr.replace('{{verb}}', finalWords.verbs[i]);
+    storyStr = storyStr.replace('{{adjective}}', finalWords.adjectives[i]);
+    storyStr = storyStr.replace('{{adverb}}', finalWords.adverbs[i]);
+  }
+  return storyStr;
 }

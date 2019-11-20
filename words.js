5940db14316633801ca66c7550873aff98fe7581
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2019-11-19 21:01:19 -0700

    added new words.js file

---
+++ words.js
@@ -0,0 +1,6 @@
+var backupWords = {
+  nouns: ['peacock', 'globe', 'flask', 'kumquat', 'peninusla', 'corset'],
+  verbs: ['toil', 'circumnavigate', 'fracture', 'melt', 'percolate', 'deceive'],
+  adjectives: ['ornamental', 'flatulant', 'squishy', 'bedraggled', 'flexible', 'churlish'],
+  adverbs: ['lovingly', 'lazily', 'quickly', 'joyously', 'passionately', 'adamantly']
+};

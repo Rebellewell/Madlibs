57e75ee266160e36b778f629fe308e10f73984f0
Rebellewell <56415164+Rebellewell@users.noreply.github.com>
2019-11-19 18:07:15 -0700

    storyHandler function and storyBtn logs array of noun inputs to console

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var storyBtn = document.getElementById('generate-story-button');
 var storyBox = document.getElementById('story-container');
 var wordInput = document.getElementsByClassName('word-type-select');
-var nounList = document.getElementById('nouns').value; 
+var nounInput = document.getElementById('nouns'); 
 var words = [];
 
 storyBtn.addEventListener('click', storyHandler);
 
 function storyHandler() {
-  words.push(nounList);
-  var wordsArray = nounList.split(',');
-  console.log(wordsArray);
+  console.log(nounInput.value)
 }
